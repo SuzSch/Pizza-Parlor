3a1f05351528913e1d7d146d034458f30733d866
Suzanne Schuber <suzanneschuber@gmail.com>
2023-06-16 15:35:07 -0700

    Update HTML and WIP js.

--- js/scripts.js
+++ js/scripts.js
@@ -2,25 +2,14 @@ function Pizza() {
   this.toppings = {};
   this.size = {};
 }
-
-
-
-
 Pizza.prototype.getTopping = function () {
-  let veggiePizza = document.querySelector("input#veggie").value;
-  let plainPizza = document.querySelector("input#plain").value;
-  let pepperoniPizza = document.querySelector("input#pepperoni").value;
 
-  if (veggiePizza === "true") {
-    return value;
-  }
-  else if (plainPizza === "true") {
-    return value;
-  }
-  else if (pepperoniPizza === "true") {
-    return value;
-  }
+  if (document.getElementById('veggie').checked === true)
+    return "Veggie Pizza";
 }
+  
+  }
+
 
 
 
